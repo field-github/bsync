e211cf3b2815ffcaf465e166cc79aba47f81b094
John Everett Field <jfield@dorringtoninstruments.com>
2021-04-11 02:09:47 -0700

    added a setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bsync',
+      version='1.0',
+      description='bsync MPI thread pool',
+      author='J.E. Field',
+      author_email='jfield@dorringtoninstruments.com',
+      url='https://www.dorringtoninstruments.com',
+      packages=['bsync'],
+     )
+
